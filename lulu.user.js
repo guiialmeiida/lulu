771c264485299ad43d54a47ac4f5ae47f00b852d
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2019-08-08 14:19:37 -0300

    Add files via upload

---
+++ lulu.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         lulucage
+// @version      1.0
+// @description  Luan
+// @author       Thauã Corrêa Martins
+// @source       https://github.com/correamth
+// @match        *
+// @require      https://raw.githubusercontent.com/correamth/lulu/master/lulu.js
+// ==/UserScript==
