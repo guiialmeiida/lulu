9a808ff4b816016edf728e8e3444f6dc98ddb073
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2019-08-08 14:20:55 -0300

    Update lulu.user.js

--- lulu.user.js
+++ lulu.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         lulucage
-// @version      1.0
+// @version      1.1
 // @description  Luan
 // @author       Thauã Corrêa Martins
 // @source       https://github.com/correamth
